f2d717b4baab60bdd1a3563ce7e849d37d03f07e
bersLucas <Lucas@NeilSanta-Anna.local>
2016-11-11 11:05:13 -0500

    Initial

---
+++ js/HI6MA-vue.js
@@ -0,0 +1,85 @@
+var domain = document.domain;
+if (domain.substr(0, 3) === "www") {
+  domain = domain.substr(4, domain.length);
+}
+  
+var HI6MA = new Vue({
+  el: '#menu',
+  
+  data: {
+    
+    //Array of all books
+    booklist: books,
+    
+    //Display states
+    openChapter: false,
+    readingBook: false,
+    
+    //Text variables
+    series_full: "",
+    series: "",
+    chapter: "",
+    chapter_full: "",
+    domain: domain,
+    
+    //Chapter list
+    loadedChapters: new Array(),
+    
+  },
+  methods: {
+    //Fetch covers
+    bookCover: function(id) {
+      var cover = "i/" + this.booklist[id].folder;
+      cover += "/cover.jpg";
+      return cover;
+    },
+    
+    //click on a book
+    openBook: function(id){
+      this.openChapter = true;
+      this.loadedChapters = this.booklist[id].chapters;
+      this.series = this.booklist[id].folder;
+      this.series_full = this.booklist[id].name;
+    },
+    
+    //back button
+    closeBook: function(){
+      this.openChapter = false;
+      this.loadedChapters = [""];
+    },
+    
+    //Load a chapter
+    loadChapter: function(id){
+      this.chapter_full = this.loadedChapters[id];
+      var chapterID = this.loadedChapters[id];
+      this.chapter = chapterID.replace(/\W/g, '');
+      
+      this.readingBook = true;
+      this.openChapter = false;
+      
+      loadBook();
+      readingEvents();
+      window.scrollTo(0,0);
+    },
+    
+    //Return to menu
+    returnToSeriesList: function(){
+      this.readingBook = false;
+      location.hash = ""
+    },
+    
+    //remove non alpha-numberic characters
+    truncateChapter: function(id){
+      var chapterID = this.loadedChapters[id];
+      return chapterID.replace(/\W/g, '');
+    },
+    
+    checkEmpty: function(variable){
+      if(variable == undefined || variable == ""){
+        return false;
+      }else{
+        return true;
+      }
+    }
+  }
+});
